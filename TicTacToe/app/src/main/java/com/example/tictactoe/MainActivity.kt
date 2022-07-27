92468939798bf674af2d807f2caf5d87e1df453c
MegaNikitos <atikeone9@gmail.com>
2022-07-27 23:19:33 +0500

    - Добавлены 2 экрана как фрагменты: fragment_gameboard, fragment_winner_view и добавлен также контейнер для отображения этих фрагментов.
    ** Не запускается, краш-вылет без ошибок в логах

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.example.tictactoe
 
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun setActiveFragments (view : View){
+        var fragment : Fragment? = null
+
+        when (view.getId()){
+            R.id.viewGameboardBtn -> fragment = GameboardFragment()
+            R.id.viewWinnerBtn -> fragment = WinnerViewFragment()
+            else -> {}
+        }
+
+        val fragmentManager : FragmentManager = getSupportFragmentManager()
+        var fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+
+        if (fragment != null) {
+            fragmentTransaction.replace(R.id.fragment_container, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
---
+++ TicTacToe/app/src/main/java/com/example/tictactoe/WinnerViewFragment.kt
@@ -0,0 +1,34 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WinnerViewFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_winner_view, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
