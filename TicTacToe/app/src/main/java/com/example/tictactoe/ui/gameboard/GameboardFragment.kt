c760ab3026f5a862131b080388246f8d46258175
MegaNikitos <atikeone9@gmail.com>
2022-08-03 01:05:56 +0500

    - Добавили возможность победить или сыграть в ничью. - Сделали заготовку winner view на будущее.

--- TicTacToe/app/src/main/java/com/example/tictactoe/ui/gameboard/GameboardFragment.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/ui/gameboard/GameboardFragment.kt
@@ -18,8 +18,19 @@ class GameboardFragment : BaseFragment() {
 
     private var binding : FragmentGameboardBinding? = null
 
-    private lateinit var conditions: Array<CellState>
-    private lateinit var currentState: CellState
+    private lateinit var conditions : Array<CellState>
+    private lateinit var currentState : CellState
+
+    private val victoryOptions = arrayOf(
+        arrayOf(1,2,3),
+        arrayOf(4,5,6),
+        arrayOf(7,8,9),
+        arrayOf(1,5,9),
+        arrayOf(3,5,7),
+        arrayOf(1,4,7),
+        arrayOf(2,5,8),
+        arrayOf(3,6,9)
+    )
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentGameboardBinding.inflate(inflater, container, false)
@@ -48,57 +59,68 @@ class GameboardFragment : BaseFragment() {
         switchPlayer()
     }
 
-    fun onCellClick(index : Int){
-        conditions[index] = currentState
-        currentState = if (currentState == CellState.Cross) CellState.Zero else CellState.Cross
-        when (index) {
-            1 -> {
-                if(currentState == CellState.Zero) binding?.cell1?.setImageResource(R.drawable.image_zero)
-                else binding?.cell1?.setImageResource(R.drawable.image_cross)
+    private fun onCellClick(index : Int){
+
+        if(conditions[index] == CellState.None){
+            conditions[index] = currentState
+            currentState = if (currentState == CellState.Cross) CellState.Zero else CellState.Cross
+
+            when (index) {
+                1 -> {
+                    if(currentState == CellState.Zero) binding?.cell1?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell1?.setImageResource(R.drawable.image_cross)
+                }
+
+                2 -> {
+                    if(currentState == CellState.Zero) binding?.cell2?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell2?.setImageResource(R.drawable.image_cross)
+                }
+
+                3 -> {
+                    if(currentState == CellState.Zero) binding?.cell3?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell3?.setImageResource(R.drawable.image_cross)
+                }
+
+                4 -> {
+                    if(currentState == CellState.Zero) binding?.cell4?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell4?.setImageResource(R.drawable.image_cross)
+                }
+
+                5 -> {
+                    if(currentState == CellState.Zero) binding?.cell5?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell5?.setImageResource(R.drawable.image_cross)
+                }
+
+                6 -> {
+                    if(currentState == CellState.Zero) binding?.cell6?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell6?.setImageResource(R.drawable.image_cross)
+                }
+
+                7 -> {
+                    if(currentState == CellState.Zero) binding?.cell7?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell7?.setImageResource(R.drawable.image_cross)
+                }
+
+                8 -> {
+                    if(currentState == CellState.Zero) binding?.cell8?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell8?.setImageResource(R.drawable.image_cross)
+                }
+
+                9 -> {
+                    if(currentState == CellState.Zero) binding?.cell9?.setImageResource(R.drawable.image_zero)
+                    else binding?.cell9?.setImageResource(R.drawable.image_cross)
+                }
             }
 
-            2 -> {
-                if(currentState == CellState.Zero) binding?.cell2?.setImageResource(R.drawable.image_zero)
-                else binding?.cell2?.setImageResource(R.drawable.image_cross)
+            if(checkVictory()){
+                openFragment(WinnerViewFragment())
+            }
+            else if(checkNone()){
+
             }
 
-            3 -> {
-                if(currentState == CellState.Zero) binding?.cell3?.setImageResource(R.drawable.image_zero)
-                else binding?.cell3?.setImageResource(R.drawable.image_cross)
-            }
-
-            4 -> {
-                if(currentState == CellState.Zero) binding?.cell4?.setImageResource(R.drawable.image_zero)
-                else binding?.cell4?.setImageResource(R.drawable.image_cross)
-            }
-
-            5 -> {
-                if(currentState == CellState.Zero) binding?.cell5?.setImageResource(R.drawable.image_zero)
-                else binding?.cell5?.setImageResource(R.drawable.image_cross)
-            }
-
-            6 -> {
-                if(currentState == CellState.Zero) binding?.cell6?.setImageResource(R.drawable.image_zero)
-                else binding?.cell6?.setImageResource(R.drawable.image_cross)
-            }
-
-            7 -> {
-                if(currentState == CellState.Zero) binding?.cell7?.setImageResource(R.drawable.image_zero)
-                else binding?.cell7?.setImageResource(R.drawable.image_cross)
-            }
-
-            8 -> {
-                if(currentState == CellState.Zero) binding?.cell8?.setImageResource(R.drawable.image_zero)
-                else binding?.cell8?.setImageResource(R.drawable.image_cross)
-            }
-
-            9 -> {
-                if(currentState == CellState.Zero) binding?.cell9?.setImageResource(R.drawable.image_zero)
-                else binding?.cell9?.setImageResource(R.drawable.image_cross)
-            }
+            switchPlayer()
         }
-
-        switchPlayer()
     }
 
     private fun switchPlayer(){
@@ -112,6 +134,29 @@ class GameboardFragment : BaseFragment() {
             binding?.textLowerMove?.setText("Ходит Нолик!")
         }
     }
+
+    private fun checkVictory() : Boolean {
+        victoryOptions.forEach {
+            if(checkVictoryLine(it)) return true
+        }
+        return false
+    }
+
+    private fun checkVictoryLine(row : Array<Int>) : Boolean {
+        val firstElement = conditions[row[0]]
+        if(firstElement == CellState.None) return false
+
+        row.forEach {
+            if(firstElement != conditions[it]) return false
+        }
+
+        return true
+    }
+
+    private fun checkNone() : Boolean {
+        return !conditions.contains(CellState.None)
+    }
+
 }
 
 enum class CellState(@DrawableRes val icon: Int, val isClicable : Boolean) {
